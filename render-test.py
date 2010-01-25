749a279c038b245e4c378dbc68d37ae8fdc81d7c
Unknown <unknown@kiln.example.com>
2010-01-26 04:52:56 +0500

    tests for text replacement, single and multi-line

---
+++ render-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import difflib
+import os.path
+from grizz import *
+
+manifest = './test/manifest'
+root_path = os.path.dirname(manifest)
+out_path = './test/out/'
+cmp_path = './test/cmp/'
+
+with open(manifest) as f:
+    files = manifest_to_files(f)
+for f in files:
+    print('checking %s...' % f['path'])
+    with open(os.path.join(cmp_path, f['path'].lstrip('/')), 'r') as cmp:
+        for line in difflib.unified_diff(cmp.readlines(), render_file(f, files, root_path)):
+            print line,
+        print '\n'
