44ab310a8bdb246a880198acfbcecae819a7c034
Unknown <unknown@kiln.example.com>
2010-01-29 00:33:01 +0500

    Refactor text tag replacement into its own method; add URL tag replacement

--- grizz.py
+++ grizz.py
@@ -62,23 +62,27 @@ def render_file(file, files, root_path):
         except IOError: # included file not found
             print('''error: referenced template in %s not found; see following error''' % file['template'])
             raise
+        
+        try:
+            lines = replace_text_tags(lines, file, root_path)
+        except IOError: # included file not found
+            print('''error: referenced content file in %s not found; see following error''' % file['path'])
+            raise
+        except KeyError as e: # content tag not found in manifest
+            print('''warning: content tag %s found in %s, but no replacement file is specified''' % (e, file['path']))
+
         for line in lines:
-            m = re.search(r'{(?P<name>\w+)}', line)
+            m = re.search(r'{\@(?P<name>\w+)}', line)
             if m:
+                span = m.span()
                 try:
-                    span = m.span()
-                    with open(os.path.join(root_path, file['content'][m.group('name')])) as content:
-                        content_lines = content.readlines()
-                        content_lines[0] = line[:span[0]] + content_lines[0]
-                        content_lines[-1] = content_lines[-1].rstrip('\n') + line[span[1]:]
-                        ret += content_lines
-                except:
-                    print('''warning: found content %s in template %s, but there's no associated file''' % (m.group('name'), file['template']))
-                    ret.append(line)
-            else:
-                ret.append(line)
+                    url = [file['path'] for file in files if 'name' in file and file['name'] == m.group('name')][0]
+                    line = line[:span[0]] + url + line[span[1]:]
+                except IndexError:
+                    print('''warning: referenced URL %s in %s not found''' % (m.group('name'), file['path']))
+            ret.append(line)
     return ret
-    
+
 def replace_template_tags(lines, root_path):
     """replaces all {/path/to/template} tags with the text of the template, with the same replacement performed on the template. paths are relative to root_path, even if prefixed with /."""
     ret = []
@@ -93,5 +97,21 @@ def replace_template_tags(lines, root_path):
                 template_lines[-1] = template_lines[-1].rstrip('\n') + line[span[1]:]
                 ret += template_lines
         else:            
+            ret.append(line)
+    return ret
+
+def replace_text_tags(lines, file, root_path):
+    """replaces all {name} tags with the associated text, given the information in file."""
+    ret = []
+    for line in lines:
+        m = re.search(r'{(?P<name>\w+)}', line)
+        if m:
+            span = m.span()
+            with open(os.path.join(root_path, file['content'][m.group('name')])) as content:
+                content_lines = content.readlines()
+                content_lines[0] = line[:span[0]] + content_lines[0]
+                content_lines[-1] = content_lines[-1].rstrip('\n') + line[span[1]:]
+                ret += content_lines
+        else:
             ret.append(line)
     return ret
